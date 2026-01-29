f4ec2a76e93fa788b6585ae6aef5f29645e04a98
Jacob Finn <jacob.finn102@gmail.com>
2026-01-29 13:09:10 -0600

    Fixed assetlinks

--- src/app/.well-known/assetlinks.json/route.ts
+++ src/app/.well-known/assetlinks.json/route.ts
@@ -3,14 +3,17 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json([
     {
-      relation: ["delegate_permission/common.handle_all_urls"],
-      target: {
-        namespace: "android_app",
-        package_name: "com.streetfeast.streetfeast.dev",
-        sha256_cert_fingerprints: [
-          "2F:75:42:63:4A:9A:66:B2:18:FB:AB:75:FB:F6:1D:AB:02:BB:85:83:C5:18:3D:42:DC:DF:DF:AB:38:B0:85:15",
-        ],
-      },
+      "relation": [
+        "delegate_permission/common.handle_all_urls",
+        "delegate_permission/common.get_login_creds"
+      ],
+      "target": {
+        "namespace": "android_app",
+        "package_name": "com.streetfeast.streetfeast.dev",
+        "sha256_cert_fingerprints": [
+          "C8:D3:2C:61:72:35:5D:FE:36:58:37:E8:6F:23:D3:CB:B9:3E:A6:29:97:31:FA:58:F5:05:78:10:5D:4E:74:6E"
+        ]
+      }
     },
     {
       relation: ["delegate_permission/common.handle_all_urls"],

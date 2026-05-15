f088e6ca5f670fc04c1e2f86bdc39993f8071232
Jacob Finn <jacob.finn102@gmail.com>
2026-05-15 00:18:31 -0500

    Add EmptyCityState component

---
+++ src/components/EmptyCityState/EmptyCityState.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styles from './EmptyCityState.module.css';
+
+export default function EmptyCityState({ cityName }: { cityName: string }) {
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.heading}>Be the first food truck in {cityName}</h2>
+      <p className={styles.body}>
+        We don&apos;t have any food trucks listed in {cityName} yet. If you operate
+        a food truck or know one we should add, register and we&apos;ll get you
+        on the map.
+      </p>
+      <Link href="/register-truck" className={styles.cta}>
+        Register your food truck
+      </Link>
+    </section>
+  );
+}
